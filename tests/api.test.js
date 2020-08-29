66c8e7bc35ba815322570d5b0b4252cd6f7b72cd
SiKa <jesmv83@gmail.com>
2020-08-29 19:10:52 +0200

    test delete

--- tests/api.test.js
+++ tests/api.test.js
@@ -12,3 +12,12 @@ test('POST /tabhinchable crea documento', async() => {
     const response = await axios.post(`${BASE_URL}/api/tabhinchable`);
     expect(response.status).toBe(200);
 });
+
+test('DELETE /tabhinchable elimina tabla', async() => {
+    const response = await axios.post(`${BASE_URL}/api/tabhinchable`);
+    const id = response.data.tabHinchable._id;
+    const deleteResponse = await axios.delete(`${BASE_URL}/api/tabhinchable/${id}`);
+    expect(deleteResponse.status).toBe(200);
+});
+
+test.todo('GET /tabhinchable/id devuelve tabla')
